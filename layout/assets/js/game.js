d8e88034c731255d9d3d616f93fa183f7f22ff3e
loooxxxx <103587267+loooxxxx@users.noreply.github.com>
2023-03-29 16:07:36 +0300

    Add files via upload

---
+++ layout/assets/js/game.js
@@ -0,0 +1,26 @@
+class Game{
+    constructor (){
+        this.name = name;
+    }
+    start (){
+            this.loop();
+    }
+}
+window.onload = () => {
+    checkStorage();
+    nav();
+    startLoop();
+    setInterval(() => { 
+        if (panel === "game") {
+        game. game = new Game ();
+        game.game.start();
+        panel = "game process";
+        }
+    }, 500)
+};
+loop(){
+    requestAnimationFrame(() => {
+        this.setParams();
+        this.loop();
+    })
+}
